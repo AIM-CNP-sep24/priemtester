69936e37c47edcf6ecd940f2eb38005b609444be
Bart <bart.vanderwal@han.nl>
2024-11-28 12:57:09 +0100

    Logger gebruiken, poort nummer loggen, devContainer setup beter. Wat verbose logging onderdrukken over bytebuddy.

--- src/main/java/nl/han/devops/PriemTesterApplication.java
+++ src/main/java/nl/han/devops/PriemTesterApplication.java
@@ -1,31 +1,46 @@
 package nl.han.devops;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+import org.slf4j.*;
 import java.math.BigInteger;
 
 
 // Make this a console AND a web application
 
 @SpringBootApplication
-public class PriemTesterApplication {
+public class PriemTesterApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	private Environment environment;
+
+	private static final Logger logger = LoggerFactory.getLogger(PriemTesterApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PriemTesterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		var customPriemTester = new CustomPriemService();
-		System.out.println("Custom priemtester");
+		logger.info("Custom priemtester");
 		for(int i = 0; i<10; i++) {
-			System.out.println("Is " + i + " een priemgetal? " + customPriemTester.isPriemgetal(i));
+			logger.info("Is {} een priemgetal? {}", i, customPriemTester.isPriemgetal(i));
 		}
 
 		var mavenPriemTester = new MavenPriemService();
 
-		System.out.println("");
-		System.out.println("Maven based priemtester");
+		logger.info("");
+		logger.info("Maven based priemtester");
 
 		// BigInteger start = BigInteger.ZERO; // Start at 0
-		final BigInteger start = new BigInteger("32452842");
+		final BigInteger start = BigInteger.valueOf(32452842);
 		BigInteger increment = BigInteger.ONE; // Increment by 1
 
 		// End is 10 further.
@@ -33,8 +48,12 @@ public class PriemTesterApplication {
 		BigInteger end = start.add(diff);
 
 		for (BigInteger i = start; i.compareTo(end) <= 0; i = i.add(increment)) {
-			System.out.println("Is " + i + " een priemgetal? " + mavenPriemTester.isPriemgetal(i));
+			var isPriem = mavenPriemTester.isPriemgetal(i);
+			
+			logger.info("Is {} een priemgetal? {}", i, isPriem);
 		}
-	}
 
+		String port = environment.getProperty("server.port", "8080"); // Default to 8080 if not set
+		logger.info("Web application started on http://localhost:{}/", port);
+	}
 }
